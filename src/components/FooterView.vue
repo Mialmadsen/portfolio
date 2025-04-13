2eaec88e98223ee7e2505cebe66f17fb4509c2b9
Mialmadsen <miamad01@easv365.dk>
2025-04-13 15:25:21 +0200

    Alt skelet for alle sider sat op + pdf links + videocv

--- src/components/FooterView.vue
+++ src/components/FooterView.vue
@@ -1,7 +1,31 @@
-<template>
-    <div>   
-        Footer
+<template> 
+
+    <footer class="bg-very-grey text-very-dark !mt-40">
+        <div class="grid grid-cols-12 gap-4 h-30">
+    
+    <!-- Left Section -->
+    <div class="col-span-1 col-start-2 font-family1 text-very-dark flex flex-col justify-start !pt-4">
+        <RouterLink to="/">FORSIDE</RouterLink>
+        <RouterLink to="/about">OM MIG</RouterLink>
+        <RouterLink to="/portfolio">PORTFOLIO</RouterLink>
     </div>
+
+    <!-- Center Social Icons -->
+    <div class="col-start-11 flex items-center justify-end">
+        <a href="https://www.instagram.com/mia_lybaek_madsen/profilecard/">
+            <img alt="instagram logo" class="instagram" src="@/assets/instagram.svg" width="29" height="29" />
+        </a>  
+        <a href="https://www.linkedin.com/in/mialybaekmadsen/">
+            <img alt="linkedin logo" class="linkedin" src="@/assets/linkedin.svg" width="31" height="31" />
+        </a>
+        <a href="https://www.facebook.com/mia.madsen.3194?locale=da_DK">
+            <img alt="facebook logo" class="facebook" src="@/assets/facebook.svg" width="30" height="30" />
+        </a>
+    </div>
+
+        </div>
+    </footer>
+
 </template>
 
 <script setup>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -4,6 +4,8 @@
 
 <template>
   <main>
+
+    <!-- Hero Section -->
     <div class="relative w-full h-200 overflow-hidden">
       <!-- Hero Image -->
       <img src="../assets/hero3.jpg" alt="Home Hero Image" class="absolute top-0 left-0 w-full h-300 object-cover" />
@@ -15,5 +17,57 @@
         </h1>
       </div>
     </div>
+
+    <!-- cv & businesscard section -->
+    <div class="cv-businesscard-section flex flex-row items-center justify-center !mt-20 !mb-20">
+      <a 
+        href="/assets/CV.pdf" 
+        target="_blank" 
+        rel="noopener noreferrer" 
+        class="bg-very-grey text-butter-yellow font-family1 w-30 h-20 flex items-center justify-center !mr-10">
+        CV
+      </a>
+      <a 
+        href="/assets/.businesscard.pdf" 
+        target="_blank" 
+        rel="noopener noreferrer" 
+        class="bg-very-grey text-butter-yellow font-family1 w-30 h-20 flex items-center justify-center !ml-10">
+        Visitkort
+      </a>
+    </div>
+
+    <!-- Video cv section -->
+    <div class="video-cv-section flex justify-center items-center !mt-10">
+      <iframe width="800" height="450" src="https://www.youtube.com/embed/lAhP_2eKS8I?si=xJEzl8vM0EBMGp7K" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    </div>
+
+    <!-- Kontakt mig section -->
+    <div class="kontakt-mig-section flex flex-col items-center justify-center !mt-40">
+        <h3 class="font-family1 text-very-dark text-4xl !mb-4">
+          Kontakt Mig
+        </h3>
+        <p class="font-family2 text-very-dark text-lg !pb-4">
+          Er du interesseret i at samarbejde eller har du spørgsmål? Tøv ikke med at kontakte mig!
+        </p>
+        <p class="font-family3 text-very-dark text-lg">
+          Email: mia.l.madsen@live.dk
+        </p>
+        <p class="font-family3 text-very-dark text-lg">
+          Telefon: 26166752
+        </p>
+        <div class="socials_wrapper col-span-3 flex justify-self-center !pt-4 gap-4">
+          <a href="https://www.instagram.com/mia_lybaek_madsen/profilecard/">
+            <img alt="instagram logo" class="instagram" src="@/assets/instagram.svg" width="29" height="29" />
+          </a>  
+          <a href="https://www.linkedin.com/in/mialybaekmadsen/">
+            <img alt="linkedin logo" class="linkedin" src="@/assets/linkedin.svg" width="31" height="31" />
+          </a>
+           <a href="https://www.facebook.com/mia.madsen.3194?locale=da_DK">
+            <img alt="facebook logo" class="facebook" src="@/assets/facebook.svg" width="30" height="30" />
+           </a>
+        </div>
+      </div>
+  
+  
   </main>
 </template>
