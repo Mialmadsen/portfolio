50a6a5df2826f70272b1756779ce6cbfd2326ba0
Mialmadsen <miamad01@easv365.dk>
2025-04-23 13:27:47 +0200

    små rettelser

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -36,9 +36,9 @@ onMounted(() => {
 <template>
   <main class="overflow-hidden">
     <!-- Hero Section -->
-    <div class="relative w-full h-180 overflow-hidden">
+    <div class="relative w-full h-200 overflow-hidden">
       <!-- Hero Image -->
-      <img src="../assets/hero.png" alt="Home Hero Image" class="absolute top-0 left-0 w-full h-180 object-cover" />
+      <img src="../assets/hero.png" alt="Home Hero Image" class="absolute top-0 left-0 w-full h-200 object-cover" />
       <!-- Centered Text -->
       <div class="absolute inset-0 flex flex-col items-center justify-center text-center">
         <h1 class="font-family1 text-very-dark text-5xl !mb-4">
